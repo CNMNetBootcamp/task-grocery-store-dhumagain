0385f0f042a1fc6c6443a261beb1f35497270336
Dipendra Humagain <dhumagai@gmail.com>
2019-01-20 15:47:44 -0700

    final

---
+++ Task3/Pseudocode.cs
@@ -0,0 +1,23 @@
+//setup
+int numberOfBottles = 0;
+int response = 0;
+double unitPrice = 0.10;
+double amountPaid = 0.0;
+
+           
+//input
+ for (day 1 to day 7)
+            {
+ input("Enter number of bottles collected on Day " + day);
+
+get response;
+
+get numberOfBottles = numberOfBottles + response;
+
+            
+ //processing and output
+
+  amountPaid = numberOfBottles* unitPrice;
+
+  "Total number of bottles collected this week was " + numberOfBottles +
+                " and the total amount paid for bottles is $" + amountPaid
--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -6,7 +6,30 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //setup
+            int numberOfBottles = 0;
+            int response = 0;
+            double unitPrice = 0.10;
+            double amountPaid = 0.0;
+
+           
+            //input
+            for (int day = 1; day < 8; day++)
+            {
+                Console.WriteLine("Enter number of bottles collected on Day " + day);
+
+                response = int.Parse(Console.ReadLine());
+
+                numberOfBottles = numberOfBottles + response;
+
+            }
+            //processing and output
+            amountPaid = numberOfBottles * unitPrice;
+
+            Console.WriteLine("Total number of bottles collected this week was " + numberOfBottles +
+                " and the total amount paid for bottles is $" + amountPaid);
+            
+            Console.ReadLine();
         }
     }
 }
